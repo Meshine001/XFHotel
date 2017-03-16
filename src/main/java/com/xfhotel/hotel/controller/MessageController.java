9502a380d4379aa58fda84c6dacb289063290eea
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-03-16 11:33:30 +0800

    lock业务跑通

--- src/main/java/com/xfhotel/hotel/controller/MessageController.java
+++ src/main/java/com/xfhotel/hotel/controller/MessageController.java
@@ -6,11 +6,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,7 +30,17 @@ public class MessageController {
 	LockService lockService;
 	
 	@RequestMapping(value="/smartLock/push", method = RequestMethod.POST)
-	public @ResponseBody String push(String validate_code, String factor, String business_id, String lock_no, String pwd_no, String pwd_user_mobile, String event){
+	public @ResponseBody Map push(@RequestBody Map map){
+	//		String validate_code, String factor, String business_id, String lock_no, Integer pwd_no, String pwd_user_mobile, String event){
+	//public @ResponseBody String push(@RequestBody Map map){
+		//System.out.println(map);
+		String validate_code = (String) map.get("validate_code");
+		String factor = (String) map.get("factor");
+		String business_id = (String) map.get("business_id");
+		String lock_no = (String) map.get("lock_no");
+		Integer pwd_no = (Integer) map.get("pwd_no");
+		String pwd_user_mobile = (String) map.get("pwd_user_mobile");
+		String event = (String) map.get("event");
 		String str = Constants.LOCK_DES_KEY+factor+Constants.LOCK_ACCOUNT;
 		MessageDigest md;
 		try {
@@ -44,13 +57,15 @@ public class MessageController {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			logger.info(e.toString());
-			e.printStackTrace();
-			return e.toString();
 		}
-		return "";
+		Map obj = new HashMap();
+		obj.put("business_id", business_id);
+		obj.put("flag", 'Y');
+		obj.put("error_msg", "");
+		return obj;
 	}
 
-	public void lockSuccess(String business_id, String lock_no, String pwd_no, String pwd_user_mobile){
+	public void lockSuccess(String business_id, String lock_no, Integer pwd_no, String pwd_user_mobile){
 		int check = lockService.verify(business_id,lock_no, Integer.valueOf(pwd_no), pwd_user_mobile);
 		String pwd_text = lockService.viewPassword(pwd_user_mobile, lock_no);
 		if (check ==1 ){
@@ -61,7 +76,7 @@ public class MessageController {
 		}
 	}
 	
-	public void lockFail(String business_id, String lock_no, String pwd_no, String pwd_user_mobile){
+	public void lockFail(String business_id, String lock_no, Integer pwd_no, String pwd_user_mobile){
 		lockService.deletePassword(pwd_user_mobile, lock_no);
 	}
 }
