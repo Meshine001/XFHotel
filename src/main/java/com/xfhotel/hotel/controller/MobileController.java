08de5d1b49eaaa81d5741a8b9ef6b7732a1e4741
UserGitJason <2899398875@qq.com>
2017-04-17 17:39:39 +0800

    请求
    Signed-off-by: UserGitJason <2899398875@qq.com>

--- src/main/java/com/xfhotel/hotel/controller/MobileController.java
+++ src/main/java/com/xfhotel/hotel/controller/MobileController.java
@@ -33,7 +33,7 @@ public class MobileController {
 	@Autowired
 	CustomerService customerService;
 	
-	@RequestMapping(value = "/home",method = RequestMethod.GET)
+	@RequestMapping(value = "/home",method = RequestMethod.POST)
 	public @ResponseBody Map home(){
 		List<Room> rooms = roomService.getHomeRooms();
 		List<Map> homeRooms = new ArrayList<Map>();
