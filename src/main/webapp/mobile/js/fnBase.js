259e69967592f6893b43c8d182024f9c8234e784
Meshine001 <mingshuai001@hotmail.com>
2017-04-26 09:24:13 +0800

    挪动mobile

--- src/main/webapp/mobile/js/fnBase.js
+++ src/main/webapp/mobile/js/fnBase.js
@@ -1,10 +1,39 @@
-﻿var Constant = {
-    URL: "http://192.168.1.109:8080/hotel"
+﻿var baseUrl = getRootPath();
+var clientIp;
+$.get('https://ipinfo.io/json',function(data){
+    clientIp = data.ip;
+    //	console.log(data);
+});
+var Constant = {
+//    URL: "http://192.168.1.109:8080/hotel"
+		URL: baseUrl,
+        CLIENT_IP:clientIp
 };
+/**
+ * 获取根目录
+ * @returns {string}
+ */
+function getRootPath() {
+    //获取当前网址，如： http://localhost:8080/ems/Pages/Basic/Person.jsp
+    var curWwwPath = window.document.location.href;
+    //获取主机地址之后的目录，如： /ems/Pages/Basic/Person.jsp
+    var pathName = window.document.location.pathname;
+    var pos = curWwwPath.indexOf(pathName);
+    //获取主机地址，如： http://localhost:8080
+    var localhostPath = curWwwPath.substring(0, pos);
+    //获取带"/"的项目名，如：/ems
+    var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+    return(localhostPath + projectName+"/");
+}
+
 $(document).ready(function(){
     menuStudent()
     function menuStudent(){
-
+        //try {
+        //    throw new Date(); // 抛出当前时间对象
+        //} catch (e) {
+        //    alert(e.toLocaleString()); // 使用本地格式显示当前时间
+        //}
         $(".header-mobile .link-home").click(function(){
             $(".menuBar").addClass("menuOpen");
             $(".Masking-out").show();
@@ -18,8 +47,14 @@ $(document).ready(function(){
             setTimeout(function(){$(".Masking-out").hide()},500)
         });
     }
-
-});
+    ////轮播
+    //var mySwiper = new Swiper ('.swiper-container',{
+    //    loop: true,
+    //    autoplay: 1800,
+    //    pagination: '.swiper-pagination',
+    //    autoplayDisableOnInteraction: false
+    //});
+})
 
     //var shebei_type=0;//0网页 1安卓 2ios
     //var load_flag = false; // 数据记载标识 数据未加载
--- src/main/webapp/mobile/js/payment.js
+++ src/main/webapp/mobile/js/payment.js
@@ -1,3 +1,77 @@
 $(document).ready(function(){
-
+    payment.Entry();
 });
+var payment={
+    Entry:function(){
+        //微信支付
+        $(".wx_p").click(function () {
+
+            var url = Constant.URL + '/wechat/pay/jsOrder';
+            var data = {
+                id:'11',//订单id
+                ip:Constant.CLIENT_IP//客户端ip
+            }
+            fnBase.commonAjax(url,data,function (data) {
+                if(data.status == 'success'){
+                    callPay(
+                        data.obj.appId,
+                        data.obj.timeStamp,
+                        data.obj.nonceStr,
+                        data.obj.package,
+                        data.obj.signType,
+                        data.obj.paySign);
+                }
+            });
+        });
+    }
+};
+
+/**
+ * 调起支付
+ * @param appId
+ * @param timeStamp
+ * @param nonceStr
+ * @param package
+ * @param signType
+ * @param paySign
+ */
+function callPay(appId,timeStamp,nonceStr,package,signType,paySign) {
+    if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+        }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+        }
+    }else{
+        onBridgeReady();
+    }
+}
+
+/**
+ * jssdk
+ */
+function onBridgeReady(appId,timeStamp,nonceStr,package,signType,paySign){
+    WeixinJSBridge.invoke(
+        'getBrandWCPayRequest', {
+            "appId":appId,
+            "timeStamp":timeStamp,
+            "nonceStr":nonceStr,
+            "package":package,
+            "signType":signType,
+            "paySign":paySign
+        },
+        function(res){
+            WeixinJSBridge.log(res.err_msg);
+            if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+                console.log('支付成功');
+                //TODO 跳转支付成功页面
+
+            }else if(res.err_msg == "get_brand_wcpay_request:cancel"){//支付取消
+
+            }else {//支付失败
+
+            }
+        }
+    );
+}
